9498ddae0c7d1491fb455d0cdb0f1ac5782f5db1
Vantrex <158299250+0xVantrex@users.noreply.github.com>
2025-09-21 05:18:38 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext'; 
-import { Toaster } from 'react-hot-toast';
-import { CartProvider } from './context/CartContext'; 
-import reportWebVitals from './reportWebVitals';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import { Toaster } from "react-hot-toast";
+import { CartProvider } from "./context/CartContext";
+import reportWebVitals from "./reportWebVitals";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { SearchProvider } from "./context/SearchContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId="977444208766-m4p7qi0v5oporqpioh3cvodl3pbhauc2.apps.googleusercontent.com">
       <BrowserRouter>
-        <AuthProvider> 
-          <CartProvider>
-            <App />
-            <Toaster position="top-right" />
-          </CartProvider>
+        <AuthProvider>
+          <SearchProvider>
+            <CartProvider>
+              <App />
+              <Toaster position="top-right" />
+            </CartProvider>
+          </SearchProvider>
         </AuthProvider>
       </BrowserRouter>
     </GoogleOAuthProvider>
@@ -26,3 +29,4 @@ root.render(
 );
 
 reportWebVitals();
+
