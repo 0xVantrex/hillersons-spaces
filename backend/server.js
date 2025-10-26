55becb4237f61504570fb937079188a7efe37b94
0xVantrex <callagansikuche0@gmail.com>
2025-10-26 21:37:24 +0300

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,8 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:5173", 
-  "http://192.168.8.113:5173"
+  "http://192.168.8.113:5173",
+  "http://192.168.0.116:5173"
 ];
 
 
