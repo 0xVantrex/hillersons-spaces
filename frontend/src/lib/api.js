7a1b44d43421d1a246d40418959ae8878f840064
0xVantrex <callagansikuche0@gmail.com>
2025-11-22 06:29:05 +0300

    update

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,2 @@
 // src/lib/api.js
-const LOCALHOST_URL = import.meta.env.VITE_API_BASE_URL;
-const LAN_URL = "http://192.168.3.102:5000"; 
-
-export const API_BASE_URL =
-  window.location.hostname === "localhost" ? LOCALHOST_URL : LAN_URL;
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
